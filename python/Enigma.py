8ac15020898c90b710fc5bcd79d095d8ca396f04
Joshua Carr <rraChsoJ@gmail.com>
2020-08-05 00:55:49 -0700

    Adding imports

--- python/Enigma.py
+++ python/Enigma.py
@@ -1,44 +1,15 @@
-from random import randint
-import string
+from menu import Menu
+from alphabet import Rotor
 
 word = ""
-letters = list(input())
-
-class Alphabet:
-    def __init__(self):
-        self.alphabet = string.ascii_uppercase
-    def get_index(self, letter):
-        return self.alphabet.index(letter)
-    def get_letter(self, index):
-        return self.alphabet[index]
-
-class Rotor(Alphabet):
-    def __init__(self):
-        super().__init__()
-        self.offset = randint(0,25)
-        #make switch if Historic is active
-        self.IC = ["D","M","T","W","S","I","L","R","U","Y","Q","N","K","F","E","J","C","A","Z","B","P","G","X","O","H","V"]
-    def get_substitute(self, index):
-        index += self.offset
-        if (index > 25):
-            index -= 25
-        return index
-    def get_predefined(self,index):
-        return self.IC[index]
-
-class Menu:
-    def __init__(self):
-        print("How many rotors?")
-        self.rotorNum = int(input())
-
 menu = Menu()
 scrambler = []
 for i in range(menu.rotorNum):
     scrambler.append(Rotor())
 for rotor in scrambler:
-    for letter in letters:
+    for letter in menu.message:
         word += rotor.get_letter(rotor.get_substitute(rotor.get_index(letter.upper())))
-    letters = word
+    menu.message = word
     word = ""
-print(letters)
+print(menu.message)
 
