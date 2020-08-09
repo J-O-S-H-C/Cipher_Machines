0dc875347d64af26ada74e74207d3c83ef93087a
Joshua Carr <rraChsoJ@gmail.com>
2020-08-09 00:09:42 -0700

    minor change to make it easier to read.

--- python/Enigma.py
+++ python/Enigma.py
@@ -8,8 +8,10 @@ for i in range(menu.rotorNum):
     scrambler.append(Rotor())
 for rotor in scrambler:
     for letter in menu.message:
-        word += rotor.get_letter(rotor.get_substitute(rotor.get_index(letter.upper())))
+        word += rotor.get_letter(
+                rotor.get_substitute(
+                rotor.get_index(letter.upper())
+                ))
     menu.message = word
     word = ""
 print(menu.message)
-
